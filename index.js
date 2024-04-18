d3e90de313e548ace8e0e06a54327646fe337849
Starbel Reyes <134150161+StarbelReyes@users.noreply.github.com>
2024-04-18 13:07:27 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $('.btn-one').hover(function(){
+        $(".one").css("transform", "translateY(-10px");
+    },
+     function(){
+        $(".one").css("transform", "none")
+    });
+}); 
+
+$(document).ready(function(){
+    $('.btn-two').hover(function(){
+        $(".two").css("transform", "translateY(-10px");
+    },
+     function(){
+        $(".two").css("transform", "none")
+    });
+}); 
+
+$(document).ready(function(){
+    $('.btn-three').hover(function(){
+        $(".three").css("transform", "translateY(-10px");
+    },
+     function(){
+        $(".three").css("transform", "none")
+    });
+}); 
+
+$(document).ready(function(){
+    $('.btn-four').hover(function(){
+        $(".four").css("transform", "translateY(10px");
+    },
+     function(){
+        $(".four").css("transform", "none")
+    });
+}); 
+
+$(document).ready(function(){
+    $('.btn-five').hover(function(){
+        $(".five").css("transform", "translateY(10px");
+    },
+     function(){
+        $(".five").css("transform", "none")
+    });
+}); 
